29cf22f22b5283d6c1079f6a0ba46c36041731ac
Pashkett <pashkett@gmail.com>
2020-01-30 10:42:33 +0200

    Add project example for logging for dotnet core with NLog

---
+++ ProjcetsForTesting/SimpleLogging/SimpleLogging/Runner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NLog.Extensions.Logging;
+
+namespace SimpleLogging
+{
+    public class Runner
+    {
+        private readonly ILogger<Runner> logger;
+        public Runner(ILogger<Runner> logger) => 
+            this.logger = logger;
+
+        public void DoAction(string name) =>
+            logger.LogDebug(20, "Doing hard work! {Action}", name);
+    }
+}
