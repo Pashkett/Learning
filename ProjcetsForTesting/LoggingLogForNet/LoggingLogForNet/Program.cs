7d92b92958acaef7fd59f6fd97ec293fd44b25b7
Pashkett <pashkett@gmail.com>
2020-01-30 14:18:41 +0200

    Add new .net core project example for logging with log4net

---
+++ ProjcetsForTesting/LoggingLogForNet/LoggingLogForNet/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using log4net;
+using log4net.Config;
+using log4net.Repository;
+
+
+namespace LoggingLogForNet
+{
+    class Program
+    {
+        private static readonly ILog log = 
+            LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private static readonly ILoggerRepository logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+
+
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+            Console.WriteLine("Hello world!");
+
+            log.Info("Hello logging world!");
+            log.Error("Error!");
+            log.Warn("Warn!");
+
+            Console.ReadKey();
+        }
+    }
+}
