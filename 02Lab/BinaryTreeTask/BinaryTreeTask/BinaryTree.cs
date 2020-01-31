1f2c184574202fc98831a4194088ae2c433270dc
Pashkett <pashkett@gmail.com>
2020-01-31 11:27:42 +0200

    Add draft project for Binary Tree

---
+++ 02Lab/BinaryTreeTask/BinaryTreeTask/BinaryTree.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace BinaryTreeTask
+{
+    public class BinaryTree<T> : IEnumerable<T>
+        where T: IComparable<T>
+    {
+        private Node<T> root;
+        public BinaryTree() => root = null;
+
+        public void Insert(T data)
+        {
+            if (root == null)
+            {
+                root = new Node<T>(data);
+                return;
+            }
+
+            InsertRec(root, new Node<T>(data));
+        }
+
+        private void InsertRec(Node<T> root, Node<T> newNode)
+        {
+            if (root == null)
+                root = newNode;
+
+            if (newNode < root)
+            {
+                if (root.NodeLeft == null)
+                    root.NodeLeft = newNode;
+                else
+                    InsertRec(root.NodeLeft, newNode);
+            }
+            else
+            {
+                if (root.NodeRight == null)
+                    root.NodeRight = newNode;
+                else
+                    InsertRec(root.NodeRight, newNode);
+            }
+        }
+
+        private void DisplayTree(Node<T> root)
+        {
+            if (root == null) return;
+
+            DisplayTree(root.NodeLeft);
+            Console.Write(root.Data + " ");
+            DisplayTree(root.NodeRight);
+        }
+
+        public void DisplayTree() => DisplayTree(root);
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 02Lab/BinaryTreeTask/BinaryTreeTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BinaryTreeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary tree:");
+            BinaryTree<int> tree = new BinaryTree<int>();
+            Node<int> root = new Node<int>();
+
+            tree.Insert(4);
+            tree.Insert(2);
+            tree.Insert(5);
+            tree.Insert(1);
+            tree.Insert(3);
+            Console.WriteLine(tree.ToString());
+        }
+    }
+}
