47331fc13d4e79e1ad56911df93a8c68d85ca99d
Pashkett <pashkett@gmail.com>
2020-02-04 09:04:17 +0200

    Add Custom Array project with draft implementation.

---
+++ 02Lab/CustomArray/CustomArray/CustomArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CustomArray
+{
+    public class CustomArray<T> : ICloneable
+    {
+        public T[] Elements { get; private set; }
+        public int InitialIndex { get; private set; } = 0;
+        public int Length
+        {
+            get => Elements.Length;
+        }
+
+
+        public CustomArray() : this(0, null) { }
+        public CustomArray(params T[] elements) : this(0, elements) =>
+            Elements = elements;
+        public CustomArray(int startingIndex, params T[] elements)
+        {
+            InitialIndex = startingIndex;
+            Elements = elements;
+        }
+
+        public object Clone()
+        {
+            T[] result = new T[Length];
+            for (int i = 0; i < Elements.Length - 1; i++)
+            {
+                result[i] = Elements[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 02Lab/CustomArray/CustomArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] array = new int[5];
+        }
+    }
+}
