b3d46a6c8361813a20a65c1427a9b9566b01063e
pashkett <pashkett@gmail.com>
2020-03-23 12:16:58 +0200

    Prepare draft queries

--- ProjcetsForTesting/CodeFirstTesting/DAL/Repositories/ProductCatalogRepository.cs
+++ ProjcetsForTesting/CodeFirstTesting/DAL/Repositories/ProductCatalogRepository.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace DAL.Repositories
 {
-    public class Repository
+    public class ProductCatalogRepository
     {
     }
 }
--- ProjcetsForTesting/CodeFirstTesting/DAL/SeedingDataExtension/ProductsContextExtension.cs
+++ ProjcetsForTesting/CodeFirstTesting/DAL/SeedingDataExtension/ProductsContextExtension.cs
@@ -5,53 +5,87 @@ using System.Text;
 using System.Text.Json;
 using System.IO;
 using DAL.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DAL.SeedingDataExtension
 {
     public static class ProductsContextExtension
     {
-        public static void SeedData(this ProductsContext productsContext)
+        public static void SeedData<T>(this ProductsContext productsContext, DbSet<T> set, string fileName)
+            where T : class
         {
-            
-            if(productsContext.Suppliers.Count() == 0
-                && File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "Suppliers.json")))
+            if (set.Count() == 0 && File.Exists(Path.Combine(Directory.GetCurrentDirectory(), fileName)))
             {
-                    string file = File.ReadAllText("Suppliers.json");
-
-                    var data = JsonSerializer.Deserialize<List<Supplier>>(file);
+                try
+                {
+                    string file = File.ReadAllText(fileName);
+                    var data = JsonSerializer.Deserialize<List<T>>(file);
                     productsContext.AddRange(data);
-
                     productsContext.SaveChanges();
-            }
-
-            if (productsContext.Categories.Count() == 0 
-                && File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "Categories.json")))
-            {
-                
-                string file = File.ReadAllText("Categories.json");
-
-                var data = JsonSerializer.Deserialize<List<Category>>(file);
-                productsContext.AddRange(data);
-                productsContext.SaveChanges();
-            }
-
-            if (productsContext.Products.Count() == 0
-                && File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "Products.json")))
-            {
-                string file = System.IO.File.ReadAllText("Products.json");
-                var data = JsonSerializer.Deserialize<List<Product>>(file);
-                productsContext.AddRange(data);
-                productsContext.SaveChanges();
-            }
-
-            if (productsContext.ProductCategories.Count() == 0
-                && File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "ProductCategories.json")))
-            {
-                string file = System.IO.File.ReadAllText("ProductCategories.json");
-                var data = JsonSerializer.Deserialize<List<ProductCategory>>(file);
-                productsContext.AddRange(data);
-                productsContext.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
         }
     }
 }
+
+            //"Suppliers.json"
+            //"Categories.json"
+            //"Products.json"
+            //"ProductCategories.json"
+
+            //if (productsContext.Suppliers.Count() == 0
+            //    && File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "Suppliers.json")))
+            //{
+            //    try
+            //    {
+            //        string file = File.ReadAllText("Suppliers.json");
+            //        var data = JsonSerializer.Deserialize<List<Supplier>>(file);
+            //        productsContext.AddRange(data);
+            //        productsContext.SaveChanges();
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //    }
+            //}
+
+            //if (productsContext.Categories.Count() == 0
+            //    && File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "Categories.json")))
+            //{
+
+            //    try
+            //    {
+            //        string file = File.ReadAllText("Categories.json");
+            //        var data = JsonSerializer.Deserialize<List<Category>>(file);
+            //        productsContext.AddRange(data);
+            //        productsContext.SaveChanges();
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //    }
+
+            //}
+
+            //if (productsContext.ProductCategories.Count() == 0
+            //    && File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "ProductCategories.json")))
+            //{
+            //    try
+            //    {
+            //        string file = System.IO.File.ReadAllText("ProductCategories.json");
+            //        var data = JsonSerializer.Deserialize<List<ProductCategory>>(file);
+            //        productsContext.AddRange(data);
+            //        productsContext.SaveChanges();
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //    }
+            //}
+//        }
+//    }
+//}
